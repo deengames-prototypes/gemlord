f044f47e60d03be0f6f012c924e236e5ab686759
nightblade9 <nightbladecodes@gmail.com>
2020-06-30 22:12:20 -0400

    Replace GDscript project with C# project. It works!

---
+++ source/TestMap.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class TestMap : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Godot.GD.Print($"Hello from C#! It is {DateTime.Now}!");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
